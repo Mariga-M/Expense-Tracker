a2140ff423820dd923dc8e0a5844e8ae37ac59e8
Ms_Mariga <77338790+Mariga-M@users.noreply.github.com>
2024-04-09 23:09:31 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template> 
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpense />
+    <TransactionList :transactions ="transactions" />
+    <AddTransaction />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+import { ref } from 'vue';
+
+const transactions = ref([
+  { id: 1, text: "Food", amount: -20.00 },
+  { id: 2, text: "Salary", amount: 320.00 },
+  { id: 3, text: "Picnic", amount: -97.00 },
+  { id: 4, text: "Books", amount: -50.00 },
+  { id: 5, text: "Side gig", amount: 80.00 },
+])
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,62 @@
+<template>
+     <h3>History</h3>
+      <ul id="list" class="list">
+        <li 
+            v-for="transaction in transactions" 
+            :key="transaction.id" 
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{transaction.text}} <span>${{ transaction.amount }}</span>
+            <button class="delete-btn">x</button>
+        </li>
+      </ul>
+</template>
+
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true,
+    }
+})
+</script>
+
+
+<!-- composition API explicit setup -->
+<!-- <script>
+export default{
+   setup(){
+    const transactions = [
+        { id: 1, text: "Food", amount: -20.00 },
+        { id: 2, text: "Salary", amount: 320.00 },
+        { id: 3, text: "Picnic", amount: -97.00 },
+        { id: 4, text: "Books", amount: -50.00 },
+        { id: 5, text: "Side gig", amount: 80.00 },
+    ];
+    return{
+        transactions
+    }
+   }
+}
+</script> -->
+
+
+<!-- options API -->
+<!-- <script>
+export default{
+    data(){
+        return{
+            transactions: [
+                { id: 1, text: "Food", amount: -20.00 },
+                { id: 2, text: "Salary", amount: 320.00 },
+                { id: 3, text: "Picnic", amount: -97.00 },
+                { id: 4, text: "Books", amount: -50.00 },
+                { id: 5, text: "Side gig", amount: 80.00 },
+            ],
+        };
+    },
+}
+</script> -->
